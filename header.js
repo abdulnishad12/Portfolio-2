b34609ed7636953182fc4f3695c5dd23ff833649
翁展眉 <MayWeng@MeisMacBook.local>
2020-05-15 21:34:37 +0800

    2

--- header.js
+++ header.js
@@ -1,11 +1,11 @@
-var header='<nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">\
+var header = '<nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">\
   <a class="navbar-brand" href="index.html"><span class="h1 ml-5">May Weng</span></a>\
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">\
     <span class="navbar-toggler-icon"></span>\
   </button>\
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">\
     <div class="navbar-nav ml-auto mr-5">\
-    	<ul class="nav-item nav-link marginAuto mr-7><span style="font-size:1.3rem"><a href=Advertisement.html>Advertisement</a></span></ul>\
+    	<ul class="nav-item nav-link marginAuto mr-7><span style="font-size:1.3rem"><a href=adwork.html>Advertisement</a></span></ul>\
     	<ul class="nav-item nav-link marginAuto mr-7><span style="font-size:1.3rem"><a href=Photography.html>Photography</a></span></ul>\
     	<ul class="nav-item nav-link marginAuto mr-7><span style="font-size:1.3rem"><a href=Illustration.html>Illustration</a></span></ul>\
     	<ul class="nav-item nav-link marginAuto mr-7><span style="font-size:1.3rem"><a href=LOGOs.html>LOGOs</span></a></ul>\
